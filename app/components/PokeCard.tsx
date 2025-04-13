964080c2a8219451fa099b32b4239352ca0788df
ahmed-morshedy <ahmedmorshedy270@yahoo.com>
2025-04-13 05:31:07 +0200

    update Pokemon Card

--- app/components/PokeCard.tsx
+++ app/components/PokeCard.tsx
@@ -11,7 +11,7 @@ function PokeCard({ pokemon }: Props) {
   const img = pokemon.sprites.other.home.front_default;
   return (
     <Link href={`/pokemon/${id}`}>
-      <div className=" relative shadow rounded-xl flex justify-center items-center flex-col  w-[95px] md:w-[150px] h-[140px] md:h-[220px] overflow-hidden ">
+      <div className=" relative shadow rounded-xl  w-[95px] md:w-[150px] h-[140px] md:h-[220px] overflow-hidden ">
         <span className=" absolute top-1 right-1 text-medium text-sm">{`#${id}`}</span>
 
         <Image
@@ -19,7 +19,7 @@ function PokeCard({ pokemon }: Props) {
           width={150}
           height={150}
           alt={`pokemon ${name} image`}
-          className=" absolute z-10"
+          className=" absolute z-10 mt-4"
         />
 
         <div className="p-1 bg-light rounded-xl w-full h-[25px] md:h-[35px] bottom-0 absolute z-0 flex justify-center items-center ">
