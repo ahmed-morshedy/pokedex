b5be630ad9637a385dec729ec19a329d1abc2fa7
ahmed-morshedy <ahmedmorshedy270@yahoo.com>
2025-04-13 04:55:05 +0200

    update pokeball

--- app/components/PokePage.tsx
+++ app/components/PokePage.tsx
@@ -5,21 +5,26 @@ import Link from "next/link";
 
 const PokePage = ({ poke }: Props) => {
   const { name, id, types, stats, abilities, sprites } = poke || {};
-  // const img = poke.sprites.front_default;
+
   return (
     <div className="relative  h-full w-full ">
-      <div className="absolute top-5 left-0 w-full h-full -z-10">
-        <img src="/assets/pokeball_2.png" alt="Pokeball" className="" />
-      </div>
+      <img
+        src="/assets/pokeball_2.png"
+        alt="Pokeball"
+        className="absolute top-7 right-0"
+      />
+
       <div className="p-2 mt-6 flex justify-between items-center w-full px-4 text-white">
         <div className="flex items-center">
           <Link href="/" className="hover:-translate-x-2 transition-all">
             <img src="/assets/Vector_2.svg" alt="Back to home" />
           </Link>
+
           <p className="font-bold text-2xl ml-2">{name}</p>
         </div>
         <span className="font-bold text-2xl">{`#${id}`}</span>
       </div>
+
       <div
         className={` relative m-2 mt-[160px] rounded-3xl bg-white p-3 flex flex-col items-center`}
       >
