6f4b3c3437cb4adcd21033acb5204fa952b10bbc
ahmed-morshedy <ahmedmorshedy270@yahoo.com>
2025-04-15 02:51:23 +0200

    fix search bug

--- app/components/MainPage.tsx
+++ app/components/MainPage.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { fetchAPkemonByID, fetchPokemons } from "../utils/api";
 import { PokemonGridSkeleton } from "../utils/Skeletons";
 import PokeCard from "./PokeCard";
@@ -7,7 +8,7 @@ import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 
 const MainPage = () => {
-  const [Pokemons, setPokemons] = useState<PokemonDetails[]>([]);
+  const [Pokemons, setPokemons] = useState<PokemonDetails[] | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -15,9 +16,10 @@ const MainPage = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      if (searchParams.get("id")) {
+      if (Number(searchParams.get("id"))) {
         try {
-          const id = searchParams.get("id") as string;
+          error && setError(null);
+          const id = searchParams.get("id")?.trim() as string;
           setIsLoading(true);
           const data = await fetchAPkemonByID(id);
           setPokemons([data]);
@@ -41,28 +43,31 @@ const MainPage = () => {
     fetchData();
   }, [searchParams]);
 
+  console.log(searchParams.get("id"));
+
   if (isLoading) {
     return <PokemonGridSkeleton count={12} />;
   }
 
   if (error) {
     return (
-      <div className="p-4 m-4 text-center text-red-500">
+      <div className="p-4 inset-shadow-sm   bg-white mt-3 rounded-2xl justify-items-center ">
         <p>{error}</p>
-        <button
+        {/* <button
           className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
           onClick={() => window.location.reload()}
         >
           Retry
-        </button>
+        </button> */}
       </div>
     );
   }
   return (
     <div className="p-4 inset-shadow-sm grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 bg-white mt-3 rounded-2xl justify-items-center ">
-      {Pokemons.map((Pokemon) => {
-        return <PokeCard pokemon={Pokemon} key={Pokemon.id} />;
-      })}
+      {Pokemons &&
+        Pokemons.map((Pokemon) => {
+          return <PokeCard pokemon={Pokemon} key={Pokemon.id} />;
+        })}
     </div>
   );
 };
